e9eb993b92fad563af754fb709697e06ebbd1193
wiechert <marwinwiechert@icloud.com>
2016-12-26 15:48:10 +0100

    initial commit

---
+++ src/main/java/com/cleanup/CleanUpCustomerApplication.java
@@ -0,0 +1,12 @@
+package com.cleanup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanUpCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanUpCustomerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/cleanup/controller/PageController.java
@@ -0,0 +1,12 @@
+package com.cleanup.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wiechert on 26.12.16.
+ */
+@RestController
+public class PageController {
+
+
+}
